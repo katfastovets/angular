53971796fb7d8d41ceb7ef3949291c38b3a8fafa
katfastovets <tuuunes@gmail.com>
2018-05-28 15:42:14 +0300

    start typescript

--- app.js
+++ app.js
@@ -1,93 +1,104 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Student = /** @class */ (function () {
     function Student(name, surname, year) {
         this.name = name;
         this.surname = surname;
         this.year = year;
         this.marks = [];
-        this.attendance = [];
-        this.attendance.length = 25;
+        this.attendance = new Array(25);
     }
     Student.prototype.born = function () {
         return new Date().getFullYear() - this.year;
     };
     Student.prototype.marksAverage = function () {
-        var average = this.marks.reduce(function (sum, current) {
-            return sum + current;
-        });
+        var average = this.marks.reduce(function (sum, current) { return sum + current; });
         return Math.round(average / this.marks.length);
+    };
+    Student.prototype.setAttendance = function (isPresent) {
+        if (this.attendance.length > 25) {
+            return 'stack overflow';
+        }
+        for (var i = 0; i < this.attendance.length; i++) {
+            if (this.attendance[i] === undefined) {
+                this.attendance[i] = isPresent;
+                return 'ok';
+            }
+        }
+    };
+    Student.prototype.present = function () {
+        return this.setAttendance(true);
+    };
+    Student.prototype.absent = function () {
+        return this.setAttendance(false);
+    };
+    Student.prototype.attendAverage = function () {
+        return this.attendance.filter(function (item) { return item; }).length / this.attendance.length;
+    };
+    Student.prototype.summary = function () {
+        if (this.marksAverage() > 90 && this.attendAverage() > 0.9) {
+            return "Ути какой молодчинка!";
+        }
+        if (this.marksAverage() < 90 && this.attendAverage() < 0.9) {
+            return "Редиска!";
+        }
+        if (this.marksAverage() < 90 || this.attendAverage() < 0.9) {
+            return "Норм, но можно лучше";
+        }
     };
     return Student;
 }());
-function setAttendance(isPresent) {
-    if (this.attendance.length > 25) {
-        return 'stack overflow';
-    }
-    // цикл здесь потому, что с форычом item равен undefined
-    for (var i = 0; i < this.attendance.length; i++) {
-        if (this.attendance[i] === undefined) {
-            this.attendance[i] = isPresent;
-            return 'ok';
-        }
-    }
-}
-Student.prototype.present = setAttendance;
-Student.prototype.absent = setAttendance;
-Student.prototype.attendAverage = function () {
-    return this.attendance.filter(function (item) { return item; }).length / this.attendance.length;
-};
-Student.prototype.summary = function () {
-    if (this.marksAverage() > 90 && this.attendAverage() > 0.9) {
-        return "Ути какой молодчинка!";
-    }
-    if (this.marksAverage() < 90 && this.attendAverage() < 0.9) {
-        return "Редиска!";
-    }
-    if (this.marksAverage() < 90 || this.attendAverage() < 0.9) {
-        return "Норм, но можно лучше";
-    }
-};
+//using methods
 var kate = new Student('kate', 'fastovets', 1993);
 kate.marks.push(100, 99, 90);
 for (var i = 0; i < 25; i++) {
-    kate.present(true);
+    kate.present();
 }
 var nastya = new Student('nastya', 'vasilieva', 1995);
 nastya.marks.push(50, 75, 20);
 for (var i = 0; i < 23; i++) {
-    nastya.present(false);
+    nastya.present();
 }
-nastya.absent(true);
-nastya.absent(true);
+nastya.absent();
+nastya.absent();
 var vasya = new Student('vasya', 'pupkin', 1990);
 vasya.marks.push(90, 80, 45, 45, 90);
-for (var i = 0; i < 20; i++) {
-    vasya.present(true);
-}
-for (var i = 0; i < 5; i++) {
-    vasya.present(false);
-}
-function Group() { }
-Group.prototype = [];
-Group.prototype.average = function (methodName, surname) {
-    if (surname) {
-        var sortedGroup = this.slice();
-        sortedGroup.sort(function (a, b) {
-            if (a[methodName]() < b[methodName]())
-                return 1;
-        });
-        return sortedGroup.findIndex(function (item) { return item.surname === surname; }) + 1;
+var Group = /** @class */ (function (_super) {
+    __extends(Group, _super);
+    function Group() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-    else {
-        var sum = this.reduce(function (sum, item) { return sum += item[methodName](); }, 0);
-        return sum / this.length;
-    }
-};
-Group.prototype.attendance = function (surname) {
-    return this.average('attendAverage', surname);
-};
-Group.prototype.performance = function (surname) {
-    return this.average('marksAverage', surname);
-};
+    Group.prototype.average = function (methodName, surname) {
+        if (surname) {
+            var sortedGroup = this.slice();
+            sortedGroup.sort(function (a, b) {
+                if (a[methodName]() < b[methodName]()) {
+                    return 1;
+                }
+            });
+            return sortedGroup.findIndex(function (item) { return item.surname === surname; }) + 1;
+        }
+        else {
+            var sum = this.reduce(function (sum, item) { return sum += item[methodName](); }, 0);
+            return sum / this.length;
+        }
+    };
+    Group.prototype.attendance = function (surname) {
+        return this.average('attendAverage', surname);
+    };
+    Group.prototype.performance = function (surname) {
+        return this.average('marksAverage', surname);
+    };
+    return Group;
+}([]));
 var group = new Group();
 group.push(kate, nastya, vasya);
 console.log(group);
--- app.ts
+++ app.ts
@@ -1,110 +1,90 @@
 class Student {
-	name:string;
-	surname:string;
-	year:number;
-	marks;
-	attendance;
-
-	function setAttendance(isPresent) {
+	marks:number[] = [];
+	attendance:boolean[] = new Array(25);
+	constructor(public name: string,
+				public surname: string,
+				public year: number) {
+	}
+	born():number {
+		return new Date().getFullYear() - this.year;
+	}
+	marksAverage():number {
+		let average = this.marks.reduce((sum, current) => sum + current);
+		return Math.round(average / this.marks.length);
+	}
+	setAttendance(isPresent: boolean):string {
 		if (this.attendance.length > 25) {
 			return 'stack overflow';
 		}
-		// цикл здесь потому, что с форычом item равен undefined
-		for (let i = 0; i < this.attendance.length; i++) {
+		for (var i = 0; i < this.attendance.length; i++) {
 			if (this.attendance[i] === undefined) {
 				this.attendance[i] = isPresent;
 				return 'ok';
 			}
 		}
-	}	
-	constructor(name:string, surname:string, year:number) {
-		this.name = name;
-		this.surname = surname;
-		this.year = year;
-		this.marks = [];
-		this.attendance = [];
-		this.attendance.length = 25;
 	}
-	born() {
-		return new Date().getFullYear() - this.year;
+	present():string {
+		return this.setAttendance(true);
 	}
-	marksAverage() {
-		let average = this.marks.reduce(function(sum, current) {
-			return sum + current;
-		});
-		return Math.round(average / this.marks.length);
+	absent():string {
+		return this.setAttendance(false);
 	}
-}
-function setAttendance(isPresent) {
-	if (this.attendance.length > 25) {
-		return 'stack overflow';
+	attendAverage():number {
+		return this.attendance.filter(item => item).length / this.attendance.length;
 	}
-	// цикл здесь потому, что с форычом item равен undefined
-	for (let i = 0; i < this.attendance.length; i++) {
-		if (this.attendance[i] === undefined) {
-			this.attendance[i] = isPresent;
-			return 'ok';
-		}
+	summary():string {
+		if (this.marksAverage() > 90 && this.attendAverage() > 0.9) {
+			return "Ути какой молодчинка!";
+	   }
+	   if (this.marksAverage() < 90 && this.attendAverage() < 0.9) {
+		   return "Редиска!";
+	   }
+	   if (this.marksAverage() < 90 || this.attendAverage() < 0.9) {
+			return "Норм, но можно лучше";
+	   }
 	}
 }
-Student.prototype.present = setAttendance;
-Student.prototype.absent = setAttendance;
-Student.prototype.attendAverage = function() {
-	return this.attendance.filter(item => item).length / this.attendance.length; 
-}
-Student.prototype.summary = function() {
-	if (this.marksAverage() > 90 && this.attendAverage() > 0.9) {
-		return "Ути какой молодчинка!";
-	}
-	if (this.marksAverage() < 90 && this.attendAverage() < 0.9) {
-		return "Редиска!";
-	}
-	if (this.marksAverage() < 90 || this.attendAverage() < 0.9) {
-		return "Норм, но можно лучше";
-	}
-}
+
+//using methods
 let kate = new Student('kate', 'fastovets', 1993);
 kate.marks.push(100, 99, 90);
 for (let i = 0; i < 25; i++) {
-	kate.present(true);
+	kate.present();
 }
 
 let nastya = new Student('nastya', 'vasilieva', 1995);
 nastya.marks.push(50, 75, 20);
 for (let i = 0; i < 23; i++) {
-	nastya.present(false);
+	nastya.present();
 }
-nastya.absent(true);
-nastya.absent(true);
+nastya.absent();
+nastya.absent();
 
 let vasya = new Student('vasya', 'pupkin', 1990);
 vasya.marks.push(90, 80, 45, 45, 90);
-for (let i = 0; i < 20; i++) {
-	vasya.present(true);
-}
-for (let i = 0; i < 5; i++) {
-	vasya.present(false);
-}
 
-function Group() {}
-Group.prototype = [];
-Group.prototype.average = function(methodName, surname) {
-	if (surname) {
-		let sortedGroup = this.slice();
-		sortedGroup.sort(function(a, b) {
-			if (a[methodName]() < b[methodName]()) return 1;
-		})
-		return sortedGroup.findIndex(item => item.surname === surname) + 1;
-	} else {
-		let sum = this.reduce((sum, item) => sum += item[methodName](), 0);
-		return sum / this.length;	
+class Group extends [] {
+	average(methodName: string,
+			surname: string) {
+		if (surname) {
+			let sortedGroup = this.slice();
+			sortedGroup.sort(function(a, b) {
+				if (a[methodName]() < b[methodName]()) {
+					return 1;
+				}
+			});
+			return sortedGroup.findIndex(item => item.surname === surname) + 1;
+		} else {
+			let sum = this.reduce((sum, item) => sum += item[methodName](), 0);
+			return sum / this.length;	
+		}
 	}
-}
-Group.prototype.attendance = function(surname) {
-	return this.average('attendAverage', surname);
-}
-Group.prototype.performance = function(surname) {
-	return this.average('marksAverage', surname);
+	attendance(surname?: string) {
+		return this.average('attendAverage', surname);
+	}
+	performance(surname?: string) {
+		return this.average('marksAverage', surname);
+	}
 }
 let group = new Group();
 group.push(kate, nastya, vasya);
